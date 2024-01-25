275af9a5bb8b9e58967c5375d6c8855e352deb7e
Jackson-Wheeler <jackson.n.wheeler@gmail.com>
2024-01-25 08:40:18 -0800

    map of room items

--- webdriver/create-reservation.go
+++ webdriver/create-reservation.go
@@ -18,7 +18,7 @@ func createReservation(driver selenium.WebDriver, resTime myconfig.ReservationTi
 		START_TIME_INPUT_ID          = "start-time-input"
 		END_TIME_INPUT_ID            = "end-time-input"
 		SEARCH_BTN_XPATH             = `//button[normalize-space()='Search']`
-		ROOM_ITEM_CSS_SELECTOR       = ".column-text.available"
+		ROOM_ITEM_CSS_SELECTOR       = ".room-column.column"
 	)
 
 	fmt.Printf("\nCreating reservation for %s from %s to %s...\n", resTime.Date, resTime.StartTime, resTime.EndTime)
@@ -44,7 +44,7 @@ func createReservation(driver selenium.WebDriver, resTime myconfig.ReservationTi
 	// click the search button
 	myClickElement(driver, selenium.ByXPATH, SEARCH_BTN_XPATH)
 
-	time.Sleep(2 * time.Second) // TODO change to a wait for element ready
+	time.Sleep(3 * time.Second) // TODO change to a wait for element ready
 
 	// get all room items
 	// TODO get room-column items & save & print their title attribute (make struct for a room)
@@ -55,21 +55,16 @@ func createReservation(driver selenium.WebDriver, resTime myconfig.ReservationTi
 	// - else, fill out pop up window & check that selected rooms has a room in it, if not, go on to next, else, break out of loop
 	// (above four points -> function returning err, selectRoom() - error describes what went wrong)
 	roomItems := myFindElements(driver, selenium.ByCSSSelector, ROOM_ITEM_CSS_SELECTOR)
-
 	fmt.Printf("Length of room items: %d\n", len(roomItems))
 
+	roomMap := make(map[string]selenium.WebElement)
 	for _, roomItem := range roomItems {
-		text, err1 := roomItem.Text()
+		title, err1 := roomItem.GetAttribute("title")
 		if err1 != nil {
 			log.Fatal("Error: failed to get room item text - ", err1)
 		}
-		fmt.Printf("Room Item Text: %s\n", text)
-
-		style, err2 := roomItem.GetAttribute("style")
-		if err2 != nil {
-			style = ""
-		}
-		fmt.Printf("Room Item Style Attribute: %s\n", style)
+		fmt.Printf("Room Item Text: %s\n", title)
+		roomMap[title] = roomItem
 	}
 
 	// Reservation Details, button aria-label="Create a Reservation/Reservation Details"

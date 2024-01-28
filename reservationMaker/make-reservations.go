7acf970bd3f1279d82d035d389599da9d06d596f
Jackson-Wheeler <jackson.n.wheeler@gmail.com>
2024-01-27 19:26:01 -0800

    refactored create-reservation.go into make-reservations.go

--- reservationMaker/make-reservations.go
+++ reservationMaker/make-reservations.go
@@ -9,6 +9,7 @@ import (
 	"github.com/tebeka/selenium"
 )
 
+// MakeReservations makes the reservations on reservations.ucsd.edu according to the given config, using the given site credentials to login
 func MakeReservations(config myconfig.Config, siteCredentials SiteCredentials) {
 	// initialize the Selenium service & driver
 	service, driver := webdriver.InitializeWebDriver(false)
@@ -57,4 +58,37 @@ func login(driver selenium.WebDriver, siteCredentials SiteCredentials) {
 
 	// click sign in button
 	webdriver.ClickElement(driver, SIGN_IN_BTN_BY, SIGN_IN_BTN_VAL)
+}
+
+// creates a reservation for the specified time given the room preference order and reservation details
+func createReservation(driver selenium.WebDriver, resTime myconfig.ReservationTime, roomPreferenceOrder []string, reservationDetails myconfig.ReservationDetails) {
+
+	fmt.Printf("\nCreating reservation for %s from %s to %s...\n", resTime.Date, resTime.StartTime, resTime.EndTime)
+
+	// begin booking
+	beginBooking(driver, BOOKING_TYPE_STUDY_ROOM)
+
+	// set reservation time
+	setReservationTime(driver, resTime)
+
+	// select room
+	roomName, err := selectRoom(driver, roomPreferenceOrder, reservationDetails)
+	if err != nil {
+		fmt.Printf("no reservation made for %s from %s to %s - %v\n", resTime.Date, resTime.StartTime, resTime.EndTime, err)
+		return
+	}
+	fmt.Printf("selected room '%s'\n", roomName)
+
+	// Reservation Details, button aria-label="Create a Reservation/Reservation Details"
+	// event name id="event-name" - clear & send keys
+	// event type id="event-type" - just do nothing if event type is study room
+	// organization id="availablegroups" - click, down arrow twice, enter
+	// id="1stContactName" - clear & send keys
+	// id="1stContactPhone1" - clear & send keys
+	// id="1stContactEmail"	- clear & send keys
+	// choose who you are id="28" - click, down arrow three times, enter
+	// description id="27" - clear & send keys
+
+	// Create reservation button, data-bind="click: function(){ return saveReservation(); }" - click
+	// confirm reservation was made
 }
